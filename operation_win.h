c8a1819b38d3d41ba5c09a1cd638c94703a99c9b
Tony Su <peitaosu@163.com>
2016-04-11 00:21:19 +0800

    add functions declaration in header

--- operation_win.h
+++ operation_win.h
@@ -9,6 +9,20 @@ class Operation
 {
     public:
         Operation();
+
+        //mouse operation
+        void mouseMove(int x, int y);
+        void mousePress(int left_or_right);
+        void mouseRelease(int left_or_right);
+        void mouseClick(int left_or_right);
+        void mouseDClick(int left_or_right);
+        void mouseRoll();
+
+        //keyboard operation
+        void keyboardPress(char key);
+        void keyboardRelease(char key);
+        void keyboardType(char key);
+        void keyboardMType(char* multi_key);
 };
 
 #endif // OPERATION_H
