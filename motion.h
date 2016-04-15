325592051cb37da7389714e4353df3cb4ec3c983
Tony Su <peitaosu@163.com>
2016-04-15 10:57:19 +0800

    add Motion to support different type of controller

--- motion.h
+++ motion.h
@@ -1,12 +1,33 @@
 #ifndef MOTION_H
 #define MOTION_H
+#include "include/Leap.h"
 
 class Motion
 {
     public:
         Motion();
-
+        
+        bool setController(int controller_type);
+        int getControllerCurrent();
+        int* getControllerSupported();
+        
+        double getNormalizedX();
+        double getNormalizedY();
+        double getNormalizedZ();
+        
+        int getGestureStatus(int gesture_type);
+        
     private:
+        int controller_type_current;
+        int* controller_type_supported;
+        
+        double x_normalized;
+        double y_normalized;
+        double z_normalized;
+        
+        //for Leap Motion
+        Leap::Controller controller_leap;
+        Leap::InteractionBox box_leap;
         
 };
 
