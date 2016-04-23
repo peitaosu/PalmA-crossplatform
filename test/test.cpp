33913c561bc9c4302f7475f010e3d17ef7f83394
Tony Su <peitaosu@163.com>
2016-04-23 21:57:25 +0800

    add test go through in test/test

--- test/test.cpp
+++ test/test.cpp
@@ -1,21 +1,37 @@
 #include "test.h"
 #include "display.h"
-#include "include/Leap.h"
 #include "operation.h"
 #include "event.h"
 #include "foreground.h"
 #include "logger.h"
-
-#include <iostream>
+#include "process.h"
+#include "motion.h"
 Test::Test(){
 
 }
 
 void Test::testAllClasses(){
+    Process process;
+    Motion motion;
+    Operation operation;
     Display display;
-    Leap::Controller controller;
-    Leap::Listener listener;
-    controller.addListener(listener);
     Foreground foreground;
     Logger logger;
+
+}
+
+void Test::testGoThrough()
+{
+    Process process;
+    process.init();
+    process.start();
+    Motion motion;
+    motion.getControllerCurrent();
+    Operation operation;
+    operation.showDesktop();
+    Display display;
+    display.showDial("desktop", 0.5, 0.5);
+    Foreground foreground;
+    Logger logger;
+    logger.log("INFO", "TEST GO THROUGN");
 }
